bb74dcac25bffb3a2fc72e7399ec98600082b366
Sohil Chanana <schanana2003@gmail.com>
2022-11-25 16:25:33 -0500

    Added Course class, worked on timeline fragment

--- CoursePlannerApp/app/src/main/java/com/example/courseplannerapp/GeneratedTimelineActivity.java
+++ CoursePlannerApp/app/src/main/java/com/example/courseplannerapp/GeneratedTimelineActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class GeneratedTimelineActivity extends AppCompatActivity {
+
+    DatabaseReference reference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_generated_timeline);
+        reference = FirebaseDatabase.getInstance().getReference();
     }
 }
