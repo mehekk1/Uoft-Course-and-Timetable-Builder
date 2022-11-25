f47c5a428d1ce6a493ca3eb5121e8a6a4a6d1b9a
Vedat Alp Goktepe <vedatalpgktp@gmail.com>
2022-11-25 16:26:29 -0500

    Completed Design of pre-template generation page.

--- CoursePlannerApp/app/src/main/java/com/example/courseplannerapp/FutureCoursesActivity.java
+++ CoursePlannerApp/app/src/main/java/com/example/courseplannerapp/FutureCoursesActivity.java
@@ -1,10 +1,16 @@
 package com.example.courseplannerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.SearchView;
 
 public class FutureCoursesActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    
+    private SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
