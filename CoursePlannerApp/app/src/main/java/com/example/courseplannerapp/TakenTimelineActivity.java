e06689f4717db07759685dc1eb7b6e3cc89b143e
Alex Zeng <alex.zenggg@gmail.com>
2022-11-25 16:21:32 -0500

    responsive ScrollView and new layout

--- CoursePlannerApp/app/src/main/java/com/example/courseplannerapp/TakenTimelineActivity.java
+++ CoursePlannerApp/app/src/main/java/com/example/courseplannerapp/TakenTimelineActivity.java
@@ -15,7 +15,6 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-//import com.example.courseplannerapp.databinding.ActivityTakenBinding;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.ChildEventListener;
@@ -25,12 +24,9 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
-import java.util.Locale;
-import java.util.zip.Inflater;
 
 public class TakenTimelineActivity extends AppCompatActivity {
 
-    //private ActivityTakenTimeline binding;
     FirebaseDatabase db;
     DatabaseReference reference;
     String course;
@@ -51,6 +47,7 @@ public class TakenTimelineActivity extends AppCompatActivity {
         e = findViewById(R.id.course_text);
         db = FirebaseDatabase.getInstance();
         courses = new ArrayList<String>();
+        init();
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -61,7 +58,10 @@ public class TakenTimelineActivity extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<DataSnapshot> task) {
                         if(task.isSuccessful()){
-                            if (task.getResult().exists()){
+                            if(e.getText().toString().isEmpty()){
+                                Toast.makeText(context, course + "Please enter a course code", Toast.LENGTH_SHORT).show();
+                            }
+                            else if (task.getResult().exists()){
                                 Toast.makeText(context, course + " already exists", Toast.LENGTH_SHORT).show();
                             }
                             else {
@@ -85,7 +85,10 @@ public class TakenTimelineActivity extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<DataSnapshot> task) {
                         if (task.isSuccessful()) {
-                            if (task.getResult().exists()) {
+                            if(e.getText().toString().isEmpty()){
+                                Toast.makeText(context, course + "Please enter a course code", Toast.LENGTH_SHORT).show();
+                            }
+                            else if (task.getResult().exists()) {
                                 reference.child(course).removeValue();
                                 Toast.makeText(context, course + " Removed", Toast.LENGTH_SHORT).show();
                             } else {
@@ -126,115 +129,8 @@ public class TakenTimelineActivity extends AppCompatActivity {
             public void onCancelled(@NonNull DatabaseError error) {
 
             }
-
         });
     }
-    /*
-    @Override
-    public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
-        //binding = FragmentFirstBinding.inflate(inflater, container, false);
-        View itemLayout = inflater.inflate(R.layout.activity_taken_timeline, null, false);
-        b = itemLayout.findViewById(R.id.button_first);
-        b2 = itemLayout.findViewById(R.id.button_second);
-        e = itemLayout.findViewById(R.id.course_text);
-        db = FirebaseDatabase.getInstance();
-        courses = new ArrayList<String>();
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                course = e.getText().toString().toUpperCase(Locale.ROOT);
-                reference = db.getReference("Courses");
-                reference.child(course).setValue(course);
-            }
-        });
-        reference = db.getReference("Courses");
-        reference.orderByKey().addChildEventListener(new ChildEventListener() {
-            @Override
-            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-                courses.add(snapshot.getValue().toString());
-                init();
-            }
-
-            @Override
-            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-
-            }
-
-            @Override
-            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
-                courses.remove(snapshot.getValue().toString());
-                init();
-            }
-
-            @Override
-            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-
-        });
-        return itemLayout;
-    }*/
-
-    /*public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                db = FirebaseDatabase.getInstance();
-                course = e.getText().toString().toUpperCase();
-                reference = db.getReference("Courses");
-                reference.child(course).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<DataSnapshot> task) {
-                        if(task.isSuccessful()){
-                            if (task.getResult().exists()){
-                                Toast.makeText(getContext(), course + " already exists", Toast.LENGTH_SHORT).show();
-                            }
-                            else {
-                                reference.child(course).setValue(course);
-                                Toast.makeText(getContext(), course+ " Added", Toast.LENGTH_SHORT).show();
-                            }
-                        } else{
-                            Toast.makeText(getContext(), "An error has occurred", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-            }
-        });
-        b2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                db = FirebaseDatabase.getInstance();
-                course = e.getText().toString().toUpperCase();
-                reference = db.getReference("Courses");
-                reference.child(course).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<DataSnapshot> task) {
-                        if (task.isSuccessful()) {
-                            if (task.getResult().exists()) {
-                                reference.child(course).removeValue();
-                                Toast.makeText(getContext(), course + " Removed", Toast.LENGTH_SHORT).show();
-                            } else {
-                                Toast.makeText(getContext(), course + " Not In List", Toast.LENGTH_SHORT).show();
-                            }
-                        } else {
-                            Toast.makeText(getContext(), "An error has occurred", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-            }
-        });
-    }*/
-
-
 
     public void init(){
         TableLayout stk = findViewById(R.id.table_main);
@@ -245,7 +141,11 @@ public class TakenTimelineActivity extends AppCompatActivity {
         }
         TableRow tbrow0 = new TableRow(context);
         TextView tv0 = new TextView(context);
-        tv0.setText("You have taken " + courses.size() + " courses!");
+        int num = courses.size();
+        if(num==1)
+            tv0.setText("You have taken " + 1 + " course!");
+        else
+            tv0.setText("You have taken " + num + " courses!");
         tv0.setTextSize(24);
         tbrow0.addView(tv0);
         stk.addView(tbrow0);
