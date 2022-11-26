41072977c87a9e25acc9499bcb0b5f89f1678c3f
anshaneel <68671970+anshaneel@users.noreply.github.com>
2022-11-26 07:46:00 -0500

    Started coding functionality for the PageView Fragments

--- CoursePlannerApp/app/src/main/java/com/example/courseplannerapp/LoginActivity.java
+++ CoursePlannerApp/app/src/main/java/com/example/courseplannerapp/LoginActivity.java
@@ -1,14 +1,22 @@
 package com.example.courseplannerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
-public class activity_login_bg extends AppCompatActivity {
+import com.google.android.material.tabs.TabLayout;
+
+public class LoginActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager2 viewPager2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_bg);
+
+        viewPager2 = findViewById(R.id.view_pager);
     }
 }
