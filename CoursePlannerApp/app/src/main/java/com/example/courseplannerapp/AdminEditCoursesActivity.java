e18bf0ddaa2e88f99901ac4ef034c47561acaa59
AshwinM1523 <ashwinmallik@hotmail.com>
2022-11-30 15:02:54 -0500

    implementation testing

--- CoursePlannerApp/app/src/main/java/com/example/courseplannerapp/AdminEditCoursesActivity.java
+++ CoursePlannerApp/app/src/main/java/com/example/courseplannerapp/AdminEditCoursesActivity.java
@@ -3,13 +3,33 @@ package com.example.courseplannerapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class AdminEditCoursesActivity extends AppCompatActivity {
+
+    ArrayList<Course> editableCourses = new ArrayList<>();
+    FirebaseDatabase DB;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_edit_courses);
+
+    }
+
+    private void setUpCourses(){
+        DB = FirebaseDatabase.getInstance();
+        reference = DB.getReference();
+
 
     }
 }
