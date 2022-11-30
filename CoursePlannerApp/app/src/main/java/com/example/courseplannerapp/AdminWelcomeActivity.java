da5dc3ec75f0487c295a880caa4c1dcc8ab3ed37
AshwinM1523 <ashwinmallik@hotmail.com>
2022-11-29 21:49:06 -0500

    admin welcome page functionality and nav bar

--- CoursePlannerApp/app/src/main/java/com/example/courseplannerapp/AdminWelcomeActivity.java
+++ CoursePlannerApp/app/src/main/java/com/example/courseplannerapp/AdminWelcomeActivity.java
@@ -1,44 +1,143 @@
 package com.example.courseplannerapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
 public class AdminWelcomeActivity extends AppCompatActivity {
-    private Button admin_welcome_add_courses_btn;
-    private Button admin_welcome_edit_courses_btn;
+
+    private BottomNavigationView bottomAdminNav;
+    private CardView homeAdminCard;
+    private CardView libraryAdminCard;
+    private CardView courseAdminCard;
+    private CardView editAdminCard;
+    private CardView settingsAdminCard;
+    private CardView logoutAdminCard;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_welcome);
 
-        admin_welcome_add_courses_btn = findViewById(R.id.admin_welcome_add_btn);
-        admin_welcome_edit_courses_btn = findViewById(R.id.admin_welcome_edit_btn);
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        int colorCodeDark = Color.parseColor("#FF000000");
+        window.setStatusBarColor(colorCodeDark);
 
-        admin_welcome_add_courses_btn.setOnClickListener(new View.OnClickListener() {
+        bottomAdminNav = findViewById(R.id.bottom_navigation_view);
+
+        homeAdminCard = findViewById(R.id.homeAdminWidget);
+        libraryAdminCard = findViewById(R.id.libraryWidget);
+        courseAdminCard = findViewById(R.id.courseAdminWidget);
+        editAdminCard = findViewById(R.id.editAdminWidget);
+        settingsAdminCard = findViewById(R.id.settingsAdminWidget);
+        logoutAdminCard = findViewById(R.id.logoutAdminWidget);
+
+        bottomAdminNav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                openAddCoursePage();
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch (id) {
+                    case R.id.nav_home:
+                        break;
+                    case R.id.nav_timeline:
+                        openAddingCoursesPage();
+                        break;
+                    case R.id.nav_add_menu:
+                        openEditCoursesPage();
+                        break;
+                }
+                return true;
             }
         });
 
-        admin_welcome_edit_courses_btn.setOnClickListener((new View.OnClickListener() {
+        homeAdminCard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openEditCoursePage();
+                goToLink("https://github.com/AshwinM1523/b07FinalProject");
             }
-        }));
+        });
 
+        libraryAdminCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openViewAllCoursesPage();
+            }
+        });
+
+        courseAdminCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openAdminAddCoursesPage();
+            }
+        });
+
+        editAdminCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Open admin page
+            }
+        });
+
+        settingsAdminCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Open Settings
+            }
+        });
+
+        logoutAdminCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToLoginPage();
+            }
+        });
     }
-    public void openAddCoursePage(){
-        Intent intent = new Intent(this,AdminAddCourseActivity.class);
+
+    private void openAddingCoursesPage(){
+        Intent intent = new Intent(this, AdminAddCourseActivity.class);
         startActivity(intent);
     }
-    public void openEditCoursePage(){
+
+    private void openEditCoursesPage(){
+        System.out.println("page needs to be created");
     }
+
+    private void goToLink(String link){
+        Uri url = Uri.parse(link);
+        startActivity(new Intent(Intent.ACTION_VIEW, url));
+    }
+
+    private void openViewAllCoursesPage(){
+        Intent intent = new Intent(this, ViewAllCoursesActivity.class);
+        startActivity(intent);
+    }
+
+    private void openAdminAddCoursesPage(){
+        Intent intent = new Intent(this, AdminAddCourseActivity.class);
+        startActivity(intent);
+    }
+
+    private void goToLoginPage(){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
 
 }
