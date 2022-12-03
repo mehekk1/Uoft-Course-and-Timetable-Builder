7db0f37a05ad5ec1ce520e946a0c9031af9b2ce0
AshwinM1523 <ashwinmallik@hotmail.com>
2022-12-03 16:53:06 -0500

    testing reponse factor

--- CoursePlannerApp/app/src/main/java/com/example/courseplannerapp/EditCourseActivity.java
+++ CoursePlannerApp/app/src/main/java/com/example/courseplannerapp/EditCourseActivity.java
@@ -3,12 +3,16 @@ package com.example.courseplannerapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -26,17 +30,22 @@ public class EditCourseActivity extends AppCompatActivity {
 
     EditText courseEditName, courseEditCode;
     Switch switchFall, switchSummer, swichWinter;
+    Button editBtn;
+
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_course);
+        context = getApplicationContext();
 
         courseEditName = findViewById(R.id.edit_course_name_txt);
         courseEditCode = findViewById(R.id.edit_course_code_txt);
         switchFall = findViewById(R.id.edit_fall_switch);
         switchSummer = findViewById(R.id.edit_summer_switch);
         swichWinter = findViewById(R.id.edit_winter_switch);
+        editBtn = findViewById(R.id.confirm_edit_btn);
 
 
         Intent incomingIntent = getIntent();
@@ -58,6 +67,21 @@ public class EditCourseActivity extends AppCompatActivity {
                 switchSummer.setChecked(course.offerings.get(1));
                 switchFall.setChecked(course.offerings.get(2));
 
+
+                editBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if(!(swichWinter.isChecked() || switchFall.isChecked() || switchSummer.isChecked())){
+                            Toast.makeText(context, "Must have atleast one offering", Toast.LENGTH_SHORT).show();
+                        }
+
+
+                        String newCourseName = courseEditName.getText().toString();
+                        String newCourseCode = courseEditCode.getText().toString();
+
+
+                    }
+                });
 
 
             }
