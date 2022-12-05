bfab9b093c7780da1fd91f25571d5a5edc0b03cd
AshwinM1523 <ashwinmallik@hotmail.com>
2022-12-05 10:58:29 -0500

    Mockito set up

---
+++ CoursePlannerApp/app/src/test/java/com/example/courseplannerapp/LoginPresenterTest.java
@@ -0,0 +1,14 @@
+package com.example.courseplannerapp;
+
+import org.junit.Before;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoJUnitRunner;
+
+public class LoginPresenterTest {
+
+    @Before
+    public void setUpLoginPresenter(){
+        
+    }
+
+}
